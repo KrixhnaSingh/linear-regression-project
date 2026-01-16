f3484e50ce0a3211cec970b92ada48162f72b8d1
Krishna Singh <krishnarajput0990@gmail.com>
2026-01-16 17:06:43 +0530

    Implement linear regression on salary data

---
+++ linear_regression.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv("Salary_Data.csv")
+
+X = data[['YearsExperience']]
+y = data['Salary']
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = model.predict(X)
+
+plt.scatter(X, y, color='blue')
+plt.plot(X, y_pred, color='red')
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.title("Linear Regression: Salary vs Experience")
+plt.show()
+
+print("Coefficient:", model.coef_)
+print("Intercept:", model.intercept_)
